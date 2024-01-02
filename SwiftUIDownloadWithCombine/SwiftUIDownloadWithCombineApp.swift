41beda7318453b122196ca3f270c181ea5d0cde1
coding-campus <tutorial@kakao.com>
2024-01-02 16:53:24 +0900

    first commit

---
+++ SwiftUIDownloadWithCombine/SwiftUIDownloadWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDownloadWithCombineApp.swift
+//  SwiftUIDownloadWithCombine
+//
+//  Created by Jihun Kang on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDownloadWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
